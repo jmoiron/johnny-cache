c12f92a5ef8aeeb3d318af0789b4cbd1ce8aec2c
jmoiron@robin <jmoiron@robin>
2010-01-08 19:13:08 -0500

    adding localstore implementation

---
+++ johnny/localstore.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Threadlocal OpenStruct-like cache."""
+
+import threading
+
+class LocalStore(threading.local):
+    """A thread-local OpenStruct that can be used as a local cache.  This
+    module instantiates a LocalStore called 'Cache', which will be cleared
+    after every request if johnny's LocalStoreClearMiddleware is enabled.
+    It can be a thread-safe way to handle global contexts."""
+    def __init__(self, **d):
+        threading.local.__init__(self)
+        for k,v in d.iteritems():
+            threading.local.__setattr__(self, k, v)
+    # dictionary API
+    def __getitem__(self, key):
+        return self.__dict__[key]
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+    def __delitem__(self, key):
+        if key in self.__dict__:
+            del self.__dict__[key]
+    def __iter__(self):
+        return iter(self.__dict__)
+    def keys(self): return self.__dict__.keys()
+    def values(self): return self.__dict__.values()
+    def items(self): return self.__dict__.items()
+    def iterkeys(self): return self.__dict__.iterkeys()
+    def itervalues(self): return self.__dict__.itervalues()
+    def iteritems(self): return self.__dict__.iteritems()
+    def get(self, *args): return self.__dict__.get(*args)
+    def update(self, d): self.__dict__.update(d)
+    def clear(self): self.__dict__.clear()
+    def __repr__(self): return repr(self.__dict__)
+    def __str__(self): return str(self.__dict__)
+
+Cache = LocalStore()
+
