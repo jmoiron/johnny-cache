4668c3d43f0569f84275a65c4425e5896e9c3c8a
Jason Moiron <jmoiron@jmoiron.net>
2010-02-12 20:20:42 -0500

    change import path to johnny to pass johnny tests

--- johnny/tests/localstore.py
+++ johnny/tests/localstore.py
@@ -7,7 +7,7 @@ that it be tested thoroughly to make sure no changes introduce the possibility
 of stale data in the cache."""
 
 from django.test import TestCase
-import localstore
+from johnny import localstore
 
 class LocalStoreTest(TestCase):
     def test_basic_operation(self):
