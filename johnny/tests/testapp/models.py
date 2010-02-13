870b240b52cb5d76164925d820cf0af6de3867af
Jason Moiron <jmoiron@jmoiron.net>
2010-02-12 21:35:28 -0500

    add person and persontype model to the book's basic-apps testapp

--- johnny/tests/testapp/models.py
+++ johnny/tests/testapp/models.py
@@ -2,7 +2,63 @@ from django.db import models
 from django.db.models import permalink
 from django.conf import settings
 from django.contrib.auth.models import User
-from basic.people.models import Person
+#from basic.people.models import Person
+
+class PersonType(models.Model):
+    """Person type model."""
+    title = models.CharField(_('title'), max_length=100)
+    slug = models.SlugField(_('slug'), unique=True)
+
+    class Meta:
+        verbose_name = _('person type')
+        verbose_name_plural = _('person types')
+        db_table = 'people_types'
+        ordering = ('title',)
+
+    def __unicode__(self):
+        return '%s' % self.title
+
+    @permalink
+    def get_absolute_url(self):
+        return ('person_type_detail', None, {'slug': self.slug})
+
+# some details left out of the Person model, in order to avoid a requirement
+# on python-dateutil
+
+class Person(models.Model):
+    """Person model."""
+    GENDER_CHOICES = (
+        (1, 'Male'),
+        (2, 'Female'),
+    )
+    first_name = models.CharField(_('first name'), blank=True, max_length=100)
+    middle_name = models.CharField(_('middle name'), blank=True, max_length=100)
+    last_name = models.CharField(_('last name'), blank=True, max_length=100)
+    slug = models.SlugField(_('slug'), unique=True)
+    user = models.ForeignKey(User, blank=True, null=True, help_text='If the person is an existing user of your site.')
+    gender = models.PositiveSmallIntegerField(_('gender'), choices=GENDER_CHOICES, blank=True, null=True)
+    mugshot = models.FileField(_('mugshot'), upload_to='mugshots', blank=True)
+    mugshot_credit = models.CharField(_('mugshot credit'), blank=True, max_length=200)
+    birth_date = models.DateField(_('birth date'), blank=True, null=True)
+    person_types = models.ManyToManyField(PersonType, blank=True)
+    website = models.URLField(_('website'), blank=True, verify_exists=True)
+
+    class Meta:
+        verbose_name = _('person')
+        verbose_name_plural = _('people')
+        db_table = 'people'
+        ordering = ('last_name', 'first_name',)
+
+    def __unicode__(self):
+        return u'%s' % self.full_name
+
+    @property
+    def full_name(self):
+        return u'%s %s' % (self.first_name, self.last_name)
+
+    @permalink
+    def get_absolute_url(self):
+        return ('person_detail', None, {'slug': self.slug})
 
 
 class Genre(models.Model):
