ed5ebdfbac913215092c188f4ce8fc26ff351c03
Bertrand Bordage <bordage.bertrand@gmail.com>
2014-02-01 06:48:54 +0100

    Fixes Django 1.3 support.

--- johnny/tests/testapp/urls.py
+++ johnny/tests/testapp/urls.py
@@ -3,7 +3,10 @@
 
 """URLconf for Johnny's test app."""
 
-from django.conf.urls import *
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('johnny.tests.testapp.views',
    url(r'^test/template_queries', 'template_queries'),
